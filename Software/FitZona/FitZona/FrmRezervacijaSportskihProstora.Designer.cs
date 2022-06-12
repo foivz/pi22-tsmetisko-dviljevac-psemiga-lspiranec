63387d99260adddfc2d8b708443e9b15ea339665
Spockz9877 <viljevac.david@gmail.com>
2022-06-12 10:41:08 +0200

    Baza podataka
    Dodana baza podataka sa napravljenim neispunjenim tablicama
    Server Name: 31.147.204.119\PISERVER,1433
    Authentication: SQL Server Authentication
    Login: PI2224_User
    Password: ob$mxK_4
    Database: PI2224_DB

--- Software/FitZona/FitZona/FrmRezervacijaSportskihProstora.Designer.cs
+++ Software/FitZona/FitZona/FrmRezervacijaSportskihProstora.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace FitZona
 {
-    partial class Form1
+    partial class FrmRezervacijaSportskihProstora
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,18 @@ namespace FitZona
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmRezervacijaSportskihProstora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmRezervacijaSportskihProstora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rezervacija sportskih prostora";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Software/FitZona/FitZona/FrmRezervacijaSportskihProstora.cs
+++ Software/FitZona/FitZona/FrmRezervacijaSportskihProstora.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FitZona
 {
-    public partial class Form1 : Form
+    public partial class FrmRezervacijaSportskihProstora : Form
     {
-        public Form1()
+        public FrmRezervacijaSportskihProstora()
         {
             InitializeComponent();
         }
